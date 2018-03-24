2d4dacf01514b5c44839666d0525876712e181cb
fanrong zeng <fanrongzeng@fanrongzengdemacbook-pro-2.local>
2018-03-24 10:12:24 +0800

    Initial Commit

---
+++ suoyintiao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  suoyintiao
+//
+//  Created by FanrongZeng on 2018/3/24.
+//  Copyright © 2018年 FanrongZeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ suoyintiao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  suoyintiao
+//
+//  Created by FanrongZeng on 2018/3/24.
+//  Copyright © 2018年 FanrongZeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
